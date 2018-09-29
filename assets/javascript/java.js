93b240d057c2d9240867ea3188b7c8a23d9744fa
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-28 22:11:56 -0500

    initial commit, files created, firebase and moment.js linked

---
+++ assets/javascript/java.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCfs_RlvJ1cpQPAmduWion9mnpPI6of9_U",
+    authDomain: "trainz-79648.firebaseapp.com",
+    databaseURL: "https://trainz-79648.firebaseio.com",
+    projectId: "trainz-79648",
+    storageBucket: "",
+    messagingSenderId: "1059915177651"
+  };
+  firebase.initializeApp(config);
