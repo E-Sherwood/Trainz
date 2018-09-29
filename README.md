bde9fc901a15f43e6598581e662cf52ced148806
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-29 17:35:23 -0500

    hit a wall but basic functionality is there

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Trainz
+A broken webpage that is intended to hit a firebase database and update scheduled trains and keep track of arrival times via moment.js.
+~~working pieces~~
+-clock that updates the current local time and displays at the top of the page
+-firebase database updated with new train entry when user fills in input fields and hits submit
+-table updated with user input text (not firebase entry unfortunately)
+~~to be fixed later~~
+-moment.js needed to track arrival time property of trains being tracked
+-add status element that tracks whether a train has arrived or is in-transit
+-train objects from firebase need to display on page (childAdded function? couldn't figure out syntax)
+-fancier styling
--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,15 +1,16 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyCfs_RlvJ1cpQPAmduWion9mnpPI6of9_U",
     authDomain: "trainz-79648.firebaseapp.com",
     databaseURL: "https://trainz-79648.firebaseio.com",
     projectId: "trainz-79648",
     storageBucket: "",
     messagingSenderId: "1059915177651"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  var trainData = database.ref('trainz');
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var trainData = database.ref('trainz');
+//testTrain, added to database from first test
 var testTrain1 = {
     name: "THOMAS",
     destination: "Dankville",
@@ -17,11 +18,11 @@ var testTrain1 = {
     freq: "30 minutes"
 };
 //trainData.push(testTrain1);
-$('input').on("click", function(){
+$('input').on("click", function () {
     $(this).val('');
 })
-$('#submit').on("click", function(e){
-    e.preventDefault();
+$('#submit').on("click", function (ev) {
+    ev.preventDefault();
     var newTrain = {
         name: $("#name").val(),
         destination: $("#dest").val(),
@@ -29,20 +30,62 @@ $('#submit').on("click", function(e){
         freq: $("#depart").val()
     };
     trainData.push(newTrain);
-    updateFunc();
+    //pullFromDatabase();
 });
-//trainData.on('value', updateFunc);
-var updateFunc = function(){
-    var newRow = $('<tr>')
-    var test = $('<td></td>');
-     var Dest = $('<td></td>');
-     var ArriveTime = $('<td></td>');
-     var endRow = $('</tr>')
-     var Freq= $('<td></td>');
-    test.text("test");
-    Dest.text("secondTest");
-    ArriveTime.text("anotherTest");
-    Freq.text("thirdTest")
-    $("#trainTable").append(newRow, test, Dest, ArriveTime, Freq, endRow);
-    
+//look into childSnapshot
+// var updateFunc = function () {
+//     var newRow = $('<tr>');
+//     var Name = $('<td></td>');
+//     var Dest = $('<td></td>');
+//     var ArriveTime = $('<td></td>');
+//     var endRow = $('</tr>');
+//     var Freq = $('<td></td>');
+//     Name.text($("#name").val()).attr("class", "new");
+//     Dest.text($("#dest").val()).attr("class", "new");
+//     ArriveTime.text($("#arrive").val()).attr("class", "new");
+//     Freq.text($("#depart").val()).attr("class", "new");
+//     $("#trainTable").append(newRow, Name, Dest, ArriveTime, Freq, endRow);
+
+// moment.js function here -- set arrivalTime to minutes:seconds format
+// arrivalTime counts down with current clock display
+// when time matches clock, display status to "arrived" for freq minutes
+// after freq minutes set staus to "departed/in transit"
+// reset arrivalTime to default/original set time
+
+// };
+//trainData.on("childAdded", snapshot){
+    //     var newRow = $('<tr>');
+    //     var Name = $('<td></td>');
+    //     var Dest = $('<td></td>');
+    //     var ArriveTime = $('<td></td>');
+    //     var endRow = $('</tr>');
+    //     var Freq = $('<td></td>');
+    //     Name.text(trainData.name.snapshot.val());
+    //     Dest.text(trainData.destination.snapshot.val());
+    //     ArriveTime.text(trainData.arriveTime.snapshot.val());
+    //     Freq.text(trainData.freq.snapshot.val());
+    //     $("#trainTable").append(newRow, Name, Dest, ArriveTime, Freq, endRow);
+    // }
+
+moment().format('LT');
+function update() {
+    $('#clock').html(moment().format('LT'));
 }
+setInterval(update, 1000);
+
+trainData.on("value", function(snapshot) {
+    console.log(snapshot.val());
+
+    
+    //   console.log(snapshot.val().destination);
+    //   console.log(snapshot.val().arrivalTime);
+    //   console.log(snapshot.val().freq);
+    //   $("#").text(snapshot.val().name);
+    //   $("#").text(snapshot.val().destination);
+    //   $("#").text(snapshot.val().arrivalTime);
+    //   $("#").text(snapshot.val().freq);
+
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
